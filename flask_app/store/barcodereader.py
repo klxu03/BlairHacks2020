10c39f23e72c7e9a8a31b7e87bb93cd5c14aec30
Adam <39442291+Aurathic@users.noreply.github.com>
2020-02-29 22:16:46 -0500

    Random final commits

--- flask_app/store/barcodereader.py
+++ flask_app/store/barcodereader.py
@@ -1,11 +1,13 @@
 from __future__ import print_function
 import pyzbar.pyzbar as pyzbar
 import cv2
+from random import *
 
 def decode(bc) :
     # Find barcodes and QR codes
     decodedObjects = pyzbar.decode(bc)
     # Print results
+    barNum = randint(0,1000000)
     for obj in decodedObjects:
         barNum = (obj.data).decode('utf-8')
     return barNum
